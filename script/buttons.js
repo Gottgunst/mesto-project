5998b3f650131c3a66ae70f48dacea75e266716f
Vladimir Kuleshov <Gottgunst@gmail.com>
2023-04-30 00:59:55 +0300

    Объединение кнопок в один файл и заготовка под функцию по добавлению новых  слушателей

--- script/buttons.js
+++ script/buttons.js
@@ -1,3 +1,16 @@
+// LIKE buttons function
+
+const buttonsLike = document.querySelectorAll('.element__button-like');
+
+buttonsLike.forEach(button =>
+  button.addEventListener('click', imageLike)
+);
+
+function imageLike(evt) {
+ evt.target.classList.toggle('element__button-like_active');
+}
+
+
 // delete image card function
 const buttonsDel = document.querySelectorAll('.element__button-del');
 
@@ -12,4 +25,9 @@ function imageDel(evt) {
   else {
     evt.target.closest('li').remove();
   }
+}
+
+
+export function newListener() {
+
 }
