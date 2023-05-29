709faec563dea9ca33ff5ad913fd54437a58b364
Vladimir Kuleshov <gottgunst@gmail.com>
2023-05-29 13:40:17 +0300

    refactor: всплытие событий блок карточек

--- src/components/utils.js
+++ src/components/utils.js
@@ -7,3 +7,13 @@ export function sliceExt(input) {
   ext = ext.slice(ext.indexOf('_', 0),-1);
   return ext.slice(1, ext.indexOf('-',-1));
 }
+
+// ########################
+// Генерация случайного id
+// ########################
+
+export function genId() {
+  const dateString = Date.now().toString(36);
+  const randomness = Math.random().toString(36).substring(2);
+  return dateString + randomness;
+}
